37c144177c8abd7e9dbd31ded6b9d0564ac82e64
aarigela <aarigela@iu.edu>
2017-09-12 03:18:06 -0400

    First commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+nrooks.py -> Questions 1,2,3
+The code in this file uses the given abstraction and state representation.
+The function successors() and successors2() are used with is_goal().
+The function successors3() is used with is_goal2().
+
+nrooks2.py -> Question 4,5
+The code in this file uses a new abstraction and state representation.
+See PDF for detailed description about new abstraction.
+The function successors3() is used here.
+The function successors() and successors2() are not used in this new abstraction.
---
+++ nQueens.py
@@ -0,0 +1,63 @@
+# nQueens.py : Solve the N-Queens problem
+# Aaditya Arigela, September 2017
+#
+# The N-queens problem is: Given an empty NxN chessboard, place N queens on the board so that no queen
+# can attack any other, i.e. such that no two queens share the same row, column or diagonal.
+
+
+import sys
+
+# Utility function to print board in human-friendly format
+def printable_board():
+    for i in range(N):
+        for j in range(N):
+
+            # Marked unavailable
+            if initial_board[i][j] == -1:
+                print("X"),
+
+            # Queen
+            elif initial_board[i][j] == 1:
+                print("Q"),
+
+            # Empty square
+            else:
+                print("_"),
+        print
+
+def solver(row):
+    # This means all queens are placed
+    if row == N:
+        printable_board()
+        exit()
+
+    # Consider this row and try placing a queen in all columns one by one
+    for col in range(N):
+        if initial_board[row][col] != -1 and occupied_cols[col] == False and occupied_diag1[row - col + N - 1] == False and occupied_diag2[row + col] == False:
+
+            occupied_cols[col] = occupied_diag1[row - col + N - 1] = occupied_diag2[row + col] = True
+            initial_board[row][col] = 1;
+
+            solver(row + 1);
+            occupied_cols[col] = occupied_diag1[row - col + N - 1] = occupied_diag2[row + col] = False
+            initial_board[row][col] = 0;
+
+    # if queen can not be placed anywhere then return false
+    return False;
+
+def solveNQueens():
+    if solver(0) == False:
+        print("Solution does not exist for given 'N'")
+
+
+# This is N, the size of the board. It is passed through command line arguments.
+N = int(sys.argv[1])
+
+# The board is stored as a list-of-lists. Each inner list contains row and column of a queen on the board.
+# An empty list indicates no piece, and elements in the list indicates pieces on the board.
+initial_board = [[0 for i in range(N)] for j in range(N)]
+
+# Indexes
+occupied_cols = [False for i in range(N)]
+occupied_diag1 = [False for i in range(2 * N - 1)]
+occupied_diag2 = [False for i in range(2 * N - 1)]
